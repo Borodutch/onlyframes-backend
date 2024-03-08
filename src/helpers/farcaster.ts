3b0f41e9885da068b2d109ebd11071256ba39234
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-03-08 12:07:11 -0800

    Remove hubUrl from HubRestAPIClient constructor

--- src/helpers/farcaster.ts
+++ src/helpers/farcaster.ts
@@ -1,5 +1,3 @@
 import { HubRestAPIClient } from '@standard-crypto/farcaster-js'
 
-export default new HubRestAPIClient({
-  hubUrl: 'https://hub.freefarcasterhub.com:3281',
-})
+export default new HubRestAPIClient()
